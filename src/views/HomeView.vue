48137eda24657157e8f3baf7a643328ff58b58ec
OCEANO22 <n.oceano22@gmail.com>
2025-06-09 15:11:28 +0700

    perbaikan homeview.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import {Header} from '@/components/Header.vue'
-import {Navbar} from '@/components/Navbar.vue'
-import {AboutSection} from '@/components/AboutSection.vue'
-import {JobdiskSection} from '@/components/JobdiskSection.vue'
-import {ProgramKerjaTable} from '@/components/ProgramKerjaTable.vue'
-import {Footer} from '@/components/Footer.vue'
+import Header from '../components/icons/Header.vue'
+import Navbar from '../components/icons/Navbar.vue'
+import AboutSection from '../components/icons/AboutSection.vue'
+import JobdiskSection from '@/components/JobdiskSection.vue'
+import ProgramKerjaTable from '@/components/ProgramKerjaTable.vue'
+import Footer from '@/components/Footer.vue'
 import { ref,onMounted } from "vue";
 
 interface Prelation{
@@ -31,8 +31,9 @@ onMounted(() => {
     <div>
     <ul>
       <li v-for="prelation in prelations" :key="prelation.id">
-        
-      {{ prelation.name }} - {{ prelation.caption | currency }}</li>
+      <div>{{ prelation.name }}</div>
+      <div>{{ prelation.caption }}</div>
+      </li>
     </ul>
     </div>
   </main>
